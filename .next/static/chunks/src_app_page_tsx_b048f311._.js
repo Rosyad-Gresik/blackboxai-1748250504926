5516660042c4897e20a1ea092e23f624a8c53e43
Muhammad Rosyad <muhammadrosyad425@gmail.com>
2025-05-26 16:09:08 +0700

    Add .next/static/chunks/src_app_page_tsx_b048f311._.js 1748250541519

---
+++ .next/static/chunks/src_app_page_tsx_b048f311._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_components_contact-info_tsx_9c9784de._.js"
+],
+    source: "dynamic"
+});
